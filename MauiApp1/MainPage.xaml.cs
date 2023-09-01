be7b5983e88082fb7aa5fcbb91bf8486780570ee
Achraf-Assal <achrafassa.le4@gmail.com>
2023-09-02 00:36:06 +0200

    +change icon logic -handel the event click for changing the icon to save the card

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -2,16 +2,23 @@
 
 public partial class MainPage : ContentPage
 {
-
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
-	}
+    }
 
 	public void ChangeIcon(object sender, TappedEventArgs args)
 	{
-
-	}
+        var font = Icon.FontFamily;
+        if (font == "FFR")
+        {
+            Icon.FontFamily = "FFS";
+        }
+        else
+        {
+            Icon.FontFamily = "FFR";
+        }
+    }
 
 
 }
